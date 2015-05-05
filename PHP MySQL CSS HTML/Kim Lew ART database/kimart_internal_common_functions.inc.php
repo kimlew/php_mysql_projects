88b458c6bdaca93a8389a9a0cd41b8a3ef695aba
Kim Lew <gmksl7600@gmail.com>
2015-05-04 20:25:20 -0700

    Added getUserData() - And added KEY constants for user data

--- PHP MySQL CSS HTML/Kim Lew ART database/kimart_internal_common_functions.inc.php
+++ PHP MySQL CSS HTML/Kim Lew ART database/kimart_internal_common_functions.inc.php
@@ -4,9 +4,11 @@
 function mysqlQueryResult($command, $dbc) {
 // Note: If !($result), i.e., has nothing, it's not nec. a technical error but 
 // that the requested data is not in the database table.
-  $result = $dbc->query($command); 
+  $result = $dbc->query($command);
+
   if (!$result) {
-    echo "<br>Error querying database. Contact technical team.";
+  echo "Command is: " . $command . "<br>";
+    echo "<br>Query database error. Contact the technical team. <br>";
 //    die();
   }
   else {
@@ -24,7 +26,7 @@ function hasMysqlError($dbc) {
   if (!empty($mysql_error)) {
     error_log($mysql_error);
     // Note: This echo statement will NOT be in the CSS style for err_msg.
-    echo "There are technical difficulties. Please contact us.";
+    echo "Technical errors. Ask the technical team to look at error log.";
     die();
   }
 }
@@ -53,13 +55,12 @@ function displayMsg($msg_to_user) {
     echo $msg_to_user;
   }
 }
-
+/*
 function compare_input_with_db_value($string_in_superglobal, $dbc, $string_in_db) {
-/*  Check input value, sanitize string, compare one in table to one in $_POST,
-    and assign to variable if new. Return to main script for validation. 
-    Return $variable and $err_msg ideally. But I cannot return both from here
-    since you cannot return two variables. So return variable.
-*/  
+// Compare value in table to value in $_POST to see if new data was entered. If diff,
+// then there was so assign new value as return variable and return to calling script. 
+// Note: Ideal to return $variable and $err_msg BUT not allowed to return 2 variables.
+    
   if ($string_in_superglobal != $string_in_db) {
   // Case 1: Field has data and database has data and values are diff.
     return $string_in_superglobal;
@@ -69,7 +70,7 @@ function compare_input_with_db_value($string_in_superglobal, $dbc, $string_in_db
     return $string_in_db;
   }
 }
-
+*/
 function sanitize_input($dbc, $unclean_string) {
 // Sanitize an input string.
   $clean_string = mysqli_real_escape_string($dbc, (trim($unclean_string)));
@@ -94,7 +95,7 @@ function getPriceInTable($dbc, $kimart_id) {
 
 function generateDiv($div_name) {
   echo '<div class = "row">';
-  echo '<div class = "field_name_column">' . $div_name . '</div> ';
+  echo '<div class = "field_name_column"><label>' . $div_name . '</label></div> ';
   echo '<div class = "input_column">';
 }
 
@@ -110,6 +111,50 @@ function generateOptionsInArray($db_value, $options_array) {
   foreach($options_array as $index) {
     generateSelectOption($db_value, $index);
   }
+}
+
+// KEY constants for getUserData() with global scope.
+define('KEY_USER_NAME', 'user_name');
+define('KEY_EMAIL', 'email');
+define('KEY_PASSWORD', 'password');
+   
+define('KEY_CAN_DELETE_WORK', 'can_delete_work');
+define('KEY_CAN_ADD_USER', 'can_add_user');
+define('KEY_CAN_EDIT_USER', 'can_edit_user');
+define('KEY_CAN_DELETE_USER', 'can_delete_user');
+   
+define('KEY_DATE_ADDED', 'date_added');
+define('KEY_DATE_LEFT', 'date_left');
+   
+function getUserData($user_id, $dbc) {
+  // CHECK the database for user_id data. 
+  $command = "SELECT * FROM kimart_internal_login_tb WHERE user_id='$user_id'";
+  $result = mysqlQueryResult($command, $dbc);
+  hasMysqlError($dbc);
+    
+  if ($result->num_rows <= 0) {
+    $msg_to_user = "This is a new user. Click Add User.";
+    die();
+  }
+  
+  if ($result->num_rows > 0) {
+    // Retrieve data for user you are editing and add to array with string constant as key.
+    // Since adding data to array this other way, do not need array_push($userDataArray, $user_name).
+    
+    $data = $result->fetch_object();
+    $userDataArray[KEY_USER_NAME] = $data->user_name;
+    $userDataArray[KEY_EMAIL] = $data->email;
+    $userDataArray[KEY_PASSWORD] = $data->password;
+
+    $userDataArray[KEY_CAN_DELETE_WORK] = $data->can_delete_work;
+    $userDataArray[KEY_CAN_ADD_USER] = $data->can_add_user;
+    $userDataArray[KEY_CAN_EDIT_USER] = $data->can_edit_user;
+    $userDataArray[KEY_CAN_DELETE_USER] = $data->can_delete_user;
+
+    $userDataArray[KEY_DATE_ADDED] = $data->date_added;
+    $userDataArray[KEY_DATE_LEFT] = $data->date_left;
+  }
+  return $userDataArray;
 }
 
 ?>
