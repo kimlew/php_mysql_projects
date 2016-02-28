7f53b727d8da55f1b2120d8d1e61b5424e7a5080
Kim <kimlew451@gmail.com>
2016-02-28 16:42:35 -0700

    Updated to 2016

--- PHP MySQL CSS HTML/Kim Lew ART website/kimart_footer.inc.php
+++ PHP MySQL CSS HTML/Kim Lew ART website/kimart_footer.inc.php
@@ -2,9 +2,9 @@
 /*  INCLUDE file for Kim Art footer with Copyright symbol and year.   */
 ?>
 
-    <div id = "footer">
-      Copyright &#169; Kim Lew Art 2014
-    </div>
+  <div id = "footer">
+    Copyright &#169; Kim Lew Art 2016
+  </div>
   
 </body>
 </html>
